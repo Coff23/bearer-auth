ea131114c5716ed14aefec55742cb89f1a18a7f5
Joshua Coffey <itsjoshcoffey@gmail.com>
2023-05-23 17:27:03 -0500

    lab 07

--- src/auth/middleware/basic.js
+++ src/auth/middleware/basic.js
@@ -5,7 +5,9 @@ const { user } = require('../models/index.js');
 
 module.exports = async (req, res, next) => {
 
-  if (!req.headers.authorization) { return _authError(); }
+  if (!req.headers.authorization) { 
+    next('Not authorized, no token presnt!');
+   }
 
   let basic = req.headers.authorization;
   let [username, pass] = base64.decode(basic).split(':');
